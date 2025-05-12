a017e4d00f14a12ccacb242a3bebdc533f356172
Normurodov Umarjon <userpeaseonly@gmail.com>
2025-05-12 13:42:28 +0500

    Dockefile added

--- grpc_server/app/run_once_insert.py
+++ grpc_server/app/run_once_insert.py
@@ -5,8 +5,10 @@ from models import Product
 db = SessionLocal()
 for i in range(1_000_000):
     db.add(Product(name=f"Product {i}", description=f"Description {i}"))
+    print(f"Inserted product {i}", end="\r")
     if i % 1000000 == 0:
         db.commit()
+
 db.commit()
 db.close()
 print("Inserted 1,000,000 products into the database.")
