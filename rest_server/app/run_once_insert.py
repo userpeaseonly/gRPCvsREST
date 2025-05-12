4c0afc687ac0848ec6616e00b3ca5a8071e22504
Normurodov Umarjon <userpeaseonly@gmail.com>
2025-05-12 14:44:53 +0500

    run_once is updated

--- rest_server/app/run_once_insert.py
+++ rest_server/app/run_once_insert.py
@@ -5,7 +5,7 @@ from models import Product
 db = SessionLocal()
 for i in range(1_000_000):
     db.add(Product(name=f"Product {i}", description=f"Description {i}"))
-    if i % 1000000 == 0:
+    if i % 10000 == 0:
         db.commit()
 db.commit()
 db.close()
